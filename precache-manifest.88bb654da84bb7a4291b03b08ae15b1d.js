afa94786e2d25d1827fdbe5740b819f4680868a6
Kacper Łukasik <sakuy124@gmail.com>
2018-10-07 10:56:25 +0200

    Updates

---
+++ precache-manifest.88bb654da84bb7a4291b03b08ae15b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Redux-React-Task-Manager/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d1f443fdcfa55b6b2af2",
+    "url": "/Redux-React-Task-Manager/static/js/runtime~main.d1f443fd.js"
+  },
+  {
+    "revision": "8db3ee9bf3311fe64133",
+    "url": "/Redux-React-Task-Manager/static/js/main.8db3ee9b.chunk.js"
+  },
+  {
+    "revision": "4564bc49f7a8c68d9513",
+    "url": "/Redux-React-Task-Manager/static/js/1.4564bc49.chunk.js"
+  },
+  {
+    "revision": "8db3ee9bf3311fe64133",
+    "url": "/Redux-React-Task-Manager/static/css/main.31951312.chunk.css"
+  },
+  {
+    "revision": "4564bc49f7a8c68d9513",
+    "url": "/Redux-React-Task-Manager/static/css/1.1f387ef3.chunk.css"
+  },
+  {
+    "revision": "07772fbb27a90e6341f03ce97f8722f6",
+    "url": "/Redux-React-Task-Manager/index.html"
+  }
+];
